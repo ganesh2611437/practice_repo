902c413805dbc5195180761ac546c3bf3b88c9ba
Ganesh <ykganesh11@gmail.com>
2025-08-22 13:37:54 +0530

    added count function

--- practice_repo/app.py
+++ practice_repo/app.py
@@ -1,1 +1,18 @@
 print("hello world!")
+def function():
+    count=0
+    a="Ganesh"
+    for s in a:
+        if s:
+            count+=1
+    return count,len(a)
+
+def function1():
+    count=0
+    a="Ganesh"
+    for i in range(len(a)+1):
+        if i:
+            count+=1
+    return count
+
+print(function(),function1())
